2f82cae83b95b5103745c6f2bac85f05d82b2d2a
Mayra300 <143909725+Mayra300@users.noreply.github.com>
2023-09-17 15:30:53 -0500

    Add files via upload

---
+++ E1.java
@@ -0,0 +1,25 @@
+public class E1
+{
+  public static void main (String[]args)
+  {
+    // Declaramos las variables enteras x e y
+    int x = 144;
+    int y = 999;
+
+    // Mostramos por pantalla el valor de cada variable
+      System.out.println ("El valor de x es: " + x);
+      System.out.println ("El valor de y es: " + y);
+
+    // Declaramos las variables realizamos las operaciones matemeticas
+    int suma = x + y;
+    int resta = x - y;
+    float division = (float) x / y;
+    int multiplicacion = x * y;
+
+    // Mostramos los resultados por pantalla
+      System.out.println ("La suma de x e y es: " + suma);
+      System.out.println ("La resta de x e y es: " + resta);
+      System.out.println ("La division de x entre y es: " + division);
+      System.out.println ("La multiplicacion de x e y es: " + multiplicacion);
+  }
+}
