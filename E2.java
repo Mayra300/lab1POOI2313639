cc95d796aee8b8b8880f0393a9f4a6334b6b1569
Mayra300 <143909725+Mayra300@users.noreply.github.com>
2023-09-17 15:42:02 -0500

    Add files via upload

---
+++ E2.java
@@ -0,0 +1,15 @@
+public class E2
+{
+  public static void main (String[]args)
+  {
+    // Declararmos y asignamos valores a las variables
+    String nombre = "Juan Perez";
+    String direccion = "Av La marina 123";
+    String telefono = "987654321";
+
+    // Mostramos los valores en pantalla
+      System.out.println ("Nombre: " + nombre);
+      System.out.println ("Direccion: " + direccion);
+      System.out.println ("Telefono: " + telefono);
+  }
+}
